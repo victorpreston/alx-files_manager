e96ddababa9e0c6809a3e21214af55d3c665f6bd
victorpreston <prestonvictor25@gmail.com>
2024-09-01 22:01:17 +0300

    api routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
