87c1413abf13fc0bac6330677e5ce9d0d947b31c
victorpreston <prestonvictor25@gmail.com>
2024-09-01 18:25:17 +0000

    Update packages and add code coverage script

--- worker.js
+++ worker.js
@@ -66,10 +66,10 @@ userQueue.process(async (job, done) => {
     const mailContent = [
       '<div>',
       '<h3>Hello {{user.name}},</h3>',
-      'Welcome to <a href="https://github.com/B3zaleel/alx-files_manager">',
+      'Welcome to <a href="https://github.com/victorpreston/alx-files_manager">',
       'ALX-Files_Manager</a>, ',
       'a simple file management API built with Node.js by ',
-      '<a href="https://github.com/B3zaleel">Bezaleel Olakunori</a>. ',
+      '<a href="https://github.com/victorpreston">victorpreston</a>. ',
       'We hope it meets your needs.',
       '</div>',
     ].join('');
